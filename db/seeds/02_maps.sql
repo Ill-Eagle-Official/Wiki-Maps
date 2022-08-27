76d6348af1feefada3ef829c25e90376886a0055
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-08-27 23:53:23 +0000

    fix lat/lng, add seeds for users

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -2,12 +2,12 @@
 
 INSERT INTO maps (user_id, title, country, city, latitude, longitude, created_at)
 
-VALUES (1, 'Bing bongs!', 'United Kingdom', 'London', 51.5072, 0.1276, '2022-03-01'),
-       (2, 'Cool restaurants', 'Canada', 'Vancouver', 49.2872, 123.1207, '2022-05-02'),
-       (3, 'Good dispensaries', 'Canada', 'Toronto', 43.6532, 79.3832, '2022-07-03'),
-       (4, 'Nice beaches', 'United States', 'Miami', 25.7617, 80.1918, '2021-09-04'),
-       (1, 'Best coffee shops', 'United States', 'New York', 40.7128, 74.0060, '2021-11-05'),
-       (2, 'Best bars', 'Canada', 'Montreal', 45.5019, 73.5674, '2021-12-06'),
-       (3, 'Best parks', 'United States', 'Chicago', 41.8781, 87.6298, '2022-01-07'),
-       (4, 'Best landmarks', 'Brazil', 'Rio de Janeiro', 22.9068, 43.1729, '2022-02-08'),
+VALUES (1, 'Bing bongs!', 'United Kingdom', 'London', 51.5072, -0.1276, '2022-03-01'),
+       (2, 'Cool restaurants', 'Canada', 'Vancouver', 49.2872, -123.1207, '2022-05-02'),
+       (3, 'Good dispensaries', 'Canada', 'Toronto', 43.6532, -79.3832, '2022-07-03'),
+       (4, 'Nice beaches', 'United States', 'Miami', 25.7617, -80.1918, '2021-09-04'),
+       (1, 'Best coffee shops', 'United States', 'New York', 40.7128, -74.0060, '2021-11-05'),
+       (2, 'Best bars', 'Canada', 'Montreal', 45.5019, -73.5674, '2021-12-06'),
+       (3, 'Best parks', 'United States', 'Chicago', 41.8781, -87.6298, '2022-01-07'),
+       (4, 'Best landmarks', 'Brazil', 'Rio de Janeiro', -22.9068, -43.1729, '2022-02-08'),
        (3, 'Best shopping', 'Japan', 'Tokyo', 35.6762, 139.6503, '2022-04-09');
