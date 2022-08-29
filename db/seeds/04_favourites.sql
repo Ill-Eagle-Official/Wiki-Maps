ae864533bb0e8f1819d130c6924268de034f1253
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-08-29 17:55:18 +0000

    add favourite seeds

---
+++ db/seeds/04_favourites.sql
@@ -0,0 +1,8 @@
+-- Seeds for user favourites
+
+INSERT INTO favourites (user_id, map_id, created_at)
+
+VALUES (1, 2, '2022-03-02'),
+  (2, 3, '2022-03-02'),
+  (1, 5, '2022-03-02'),
+  (1, 6, '2022-03-02');
