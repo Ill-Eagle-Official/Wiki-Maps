a0847f978d2c211234f59aa1c1b0fa1afc1a6acd
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-08-29 19:20:18 +0000

    attempt routing to map page

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const mapPageRoutes = require('./routes/map_page');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -38,6 +39,8 @@ const usersRoutes = require('./routes/users');
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
+app.use('/map', mapPageRoutes);
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -46,10 +49,6 @@ app.use('/users', usersRoutes);
 
 app.get('/', (req, res) => {
   res.render('index');
-});
-
-app.get('/map', (req, res) => {
-  res.render('map_page');
 });
 
 app.listen(PORT, () => {
