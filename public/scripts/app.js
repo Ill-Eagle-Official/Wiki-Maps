7c5dfeedcf54702c2919248f22e881ec7d12c769
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-30 05:32:34 +0000

    Add link to map container that redirects to specific map page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,18 +2,18 @@
 $(document).ready(function() {
 
   const renderMaps = function(db) {
-
     for (let mapData of db) {
-
-      console.log(mapData);
-
-      $('#map-row').append(`
+      $('#map-grid').append(`
         <div id="map-box">
-          <div id="map-${mapData.city}" style="height: 200px; width: 300px; margin: 5px; border: 0.25px solid lightgray; border-radius: 8px"></div>
+          <div id="map-${mapData.id}" style="height: 300px; width: 400px; margin: 3px; border: 0.25px solid lightgray; border-radius: 10px;"></div>
+          <a id="map-box-text" style="text-decoration: none" href="http://localhost:8080/${mapData.id}">
+            <div id="map-box-text-title">${mapData.title}</div>
+            <div id="map-box-text-location"><i class="fa-solid fa-map-pin" style="color: red;"></i> ${mapData.city}, ${mapData.country}</div>
+          </a>
         </div>
       `);
 
-      let map = L.map('map-' + mapData.city).setView([mapData.latitude, mapData.longitude], 12);
+      let map = L.map('map-' + mapData.id).setView([mapData.latitude, mapData.longitude], mapData.zoom);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
@@ -28,4 +28,10 @@ $(document).ready(function() {
   };
 
   loadMaps();
+
+
+  $("#map-box").click(function() {
+    console.log("heard the click!");
+  });
+
 });
