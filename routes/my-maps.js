258f807bca047f29235b17eaa922ed5fe75e2ca4
Winnie Li <liwanyi0313@gmail.com>
2022-08-30 19:42:40 +0000

    rearranged the elements

--- routes/my-maps.js
+++ routes/my-maps.js
@@ -1,26 +1,25 @@
 /*
- * All routes for my_maps are defined here
- * Since this file is loaded in server.js into /my_aps,
- *   these routes are mounted onto /my_maps
+ * All routes for my-maps are defined here
+ * Since this file is loaded in server.js into /my-maps,
+ *   these routes are mounted onto /my-maps
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
 const express = require('express');
 const cookieSession = require("cookie-session");
 const router  = express.Router();
-const { getMyMaps } = require('../db/queries/my_maps')
+const { getMyMaps } = require('../db/queries/my-maps')
 
 router.get('/', (req, res) => {
   userId = req.session.user_id;
   getMyMaps(userId)
   .then(myMapsData => {
-    console.log("myMapsData is: ", myMapsData)
     const templateVars = {
       myMapsData
     }
-    res.render("my_maps", myMapsData);
-
-  })
+    res.render('my-maps', templateVars);
+    }
+  )
 });
 
 module.exports = router;
