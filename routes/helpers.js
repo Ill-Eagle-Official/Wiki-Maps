25a099dccc96edec355ae4e3f40af1ebf5a3fe5e
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-08-30 01:33:01 +0000

    work on showing map by id

---
+++ routes/helpers.js
@@ -0,0 +1,36 @@
+// Test map
+
+const db = require("../db/connection");
+
+const getMap = (db) => {
+  return db.query(`
+    SELECT *
+    FROM maps
+    WHERE id = 1
+    `)
+    .then(res => res.rows);
+}
+
+// Function to get map by ID from database
+const getMapByID = (db, mapID) => {
+  return db.query(`
+  SELECT *
+  FROM maps
+  WHERE id = $1
+  `, [mapID])
+  .then(res => res.rows[0])
+  .catch(err => console.log(err));
+};
+
+// Function to get all pins for a specific map using the map ID
+const getPinsByMapID = (db, mapID) => {
+  return db.query(`
+    SELECT pins.title AS title, pins.description AS description, pins.image_url AS image_url, pins.id AS id, pins.latitude AS latitude, pins.longitude AS longitude, pins.map_id AS map_id, maps.user_id AS owner
+    FROM pins
+    JOIN maps ON pins.map_id = maps.id
+    WHERE pins.map_id = $1
+    `, [mapID])
+    .then(res => res.rows);
+}
+
+module.exports = { getMap, getMapByID, getPinsByMapID };
--- routes/map_page.js
+++ routes/map_page.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const router  = express.Router();
+const { getMapByID, getPinsByMapID, getMap } = require('./helpers');
 
-router.get('/', (req, res) => {
-  res.render('map_page');
-});
+module.exports = (db) => {
+
+  router.get("/map", (req, res) => {
+    res.render("map_page");
+  });
+
+  router.get("/:id", (req, res) => {
+    const mapID = req.params.id;
+    const templateVars = {};
+
+    if (!mapID) {
+      res.status(400).send("Invalid map ID");
+      return;
+    }
+
+    const reqMapID = mapID;
+    getMapByID(db, reqMapID)
+      .then(reqMap => {
+        templateVars.map = reqMap;
+        templateVars.title = reqMap.title;
+        res.render("map_page", templateVars);
+      })
+      .catch(err => {
+        res.status(500).send("Error getting map from database");
+      });
+  });
+
+};
+
 
 module.exports = router;
