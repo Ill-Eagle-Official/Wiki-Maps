c095f90bf3f1ebbed1f68e7dd8624180108eb079
Winnie Li <liwanyi0313@gmail.com>
2022-08-31 23:12:34 +0000

    resolved conflicts

--- server.js
+++ server.js
@@ -43,10 +43,8 @@ const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
 const mapsRoutes = require('./routes/maps-api');
-const pinsRoutes = require('./routes/pins-api');
 const db = require('./db/connection');
 
-const mapsRoutes = require('./routes/maps');
 const myMapsRoutes = require('./routes/my-maps');
 
 // Mount all resource routes
@@ -57,7 +55,6 @@ app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 app.use('/api/maps', mapsRoutes(db));
 app.use('/api/maps/:id', mapsRoutes(db));
-app.use('/api/pins', pinsRoutes(db));
 
 // Note: mount other resources here, using the same pattern above
 app.use('/api/maps', mapsRoutes);
