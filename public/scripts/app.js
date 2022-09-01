8cadd9da84febfb8a0dc50b6bbfcdaf3ad006831
Ryan Fortin <ryanwfortin@gmail.com>
2022-09-01 02:52:11 +0000

    Adjust favourite button styling and click event

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,11 @@
 
 
 // Client facing scripts here
-$(document).ready(function() {
+$(document).ready(function () {
 
   let map;
 
-  const renderSingleMap = function(mapData, className) {
+  const renderSingleMap = function (mapData, className) {
 
     console.log(mapData.id)
 
@@ -15,44 +15,41 @@ $(document).ready(function() {
           <div id="${mapData.id}" class="map-box-text">
             <div id="map-box-text-title">${mapData.title}</div>
             <div id="map-box-text-location"><i class="fa-solid fa-map-pin" style="color: red;"></i> ${mapData.city}, ${mapData.country}</div>
-            <div class="favourite-icon"><i class="fa-solid fa-heart"></i></div>
-
           </div>
+           <div class="favourite-icon"><i class="fa-solid fa-heart"></i></div>
           </div>
-        </div>
       `);
 
-      map = L.map('map-' + mapData.id).setView([mapData.latitude, mapData.longitude], mapData.zoom);
+    map = L.map('map-' + mapData.id).setView([mapData.latitude, mapData.longitude], mapData.zoom);
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '© OpenStreetMap'
-      }).addTo(map);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap'
+    }).addTo(map);
 
-    }
+  }
 
-  const renderPins = function(db, mapData) {
+  const renderPins = function (db, mapData) {
 
     for (let pin of db) {
 
+      if (pin.map_id === mapData.id) {
 
-      if (pin.map_id === mapData.id){
+        let marker = L.marker([pin.latitude, pin.longitude])
+          .addTo(map)
 
-      let marker = L.marker([pin.latitude, pin.longitude])
-      .addTo(map)
-
-      marker.bindPopup(
-        `<img src=${pin.image_url} width="100" height="100"><br>
+        marker.bindPopup(
+          `<img src=${pin.image_url} width="100" height="100"><br>
         <b>${pin.title}</b><br>
         ${pin.description}`
-      ).closePopup();
+        ).closePopup();
 
+      }
     }
-  }
   };
 
 
-  const renderMaps = function(db) {
+  const renderMaps = function (db) {
 
 
     for (let mapData of db[0]) {
@@ -61,33 +58,26 @@ $(document).ready(function() {
       renderSingleMap(mapData, 'map-box-text');
       renderPins(db[1], mapData)
 
-      $(`#${mapData.id}`).click(function() {
+      $(`#${mapData.id}`).click(function () {
 
-          $('#map-grid').empty();
-          renderSingleMap(mapData, 'map-single')
-          renderPins(db[1], mapData)
-          $('#map-grid').css('display', 'flex');
+        $('#map-grid').empty();
+        renderSingleMap(mapData, 'map-single')
+        renderPins(db[1], mapData)
+        $('#map-grid').css('display', 'flex');
+      })
 
-        })
-        // console.log(mapData.id);
-      };
+    };
+    $(`.fa-heart`).click(function () {
+      console.log("Clicked on the heart icon")
+    })
 
-    }
+  }
 
-
-
-  const loadMaps = function() {
+  const loadMaps = function () {
     $.ajax('/api/maps', { method: 'GET' })
       .then(renderMaps);
   };
 
-  const loadPins = function() {
-    console.log('loadPins');
-     $.ajax('/api/pins', { method: 'GET'})
-     .then(renderPins);
-  }
-
   loadMaps();
-  loadPins();
 
 });
