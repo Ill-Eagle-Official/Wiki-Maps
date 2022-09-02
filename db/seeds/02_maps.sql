ea433d29bb9f9fb651700bc3a76d93f57ffcf665
Ryan Fortin <ryanwfortin@gmail.com>
2022-09-02 17:51:56 +0000

    Fixed a map db issue

--- db/seeds/02_maps.sql
+++ db/seeds/02_maps.sql
@@ -6,7 +6,7 @@ VALUES (1, 'Bing bongs!', 'United Kingdom', 'London', 51.5072, -0.1276, 12, '202
        (2, 'Cool restaurants', 'Canada', 'Vancouver', 49.2872, -123.1207, 12, '2022-05-02'),
        (3, 'Good dispensaries', 'Canada', 'Toronto', 43.6532, -79.3832, 12, '2022-07-03'),
        (4, 'Nice beaches', 'United States', 'Miami', 25.7617, -80.1918, 12, '2021-09-04'),
-       (1, 'Best coffee shops', 'United States', 'New York', 40.7128, -74.0060, 12, '2021-11-05'),
+       (1, 'Best coffee shops', 'United States', 'New York', 40.7128, -74.0060, 11, '2021-11-05'),
        (2, 'Best bars', 'Canada', 'Montreal', 45.5019, -73.5674, 12, '2021-12-06'),
        (3, 'Best parks', 'United States', 'Chicago', 41.8781, -87.6298, 12, '2022-01-07'),
        (4, 'Best landmarks', 'Brazil', 'Rio de Janeiro', -22.9068, -43.1729, 12, '2022-02-08'),
